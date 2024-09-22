fb9e7127a0e1681bf627e65db1d4286ba18c8f0c
MdHasanAliGHUN <mdhassanali01947@gmail.com>
2024-09-22 07:42:54 -0700

    This is my assignment four

---
+++ js.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'btn-bg-color': '#ABEF5F',
+                'bg-color': "#FFFFFF"
+            },
+            fontFamily:{
+                'manrope': "Manrope"
+
+            },  
+            backgroundImage: {
+                'bgImg': "url('/img/bg.jpg')"
+            }
+        },
+    },
+}
